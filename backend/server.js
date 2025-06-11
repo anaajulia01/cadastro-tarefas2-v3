ec03975c64348d480101639513fcbb484bde5372
DaviGab-web <d3bigithub@gmail.com>
2025-06-11 11:36:21 -0300

    Subir alterações de conexão com o banco de dados

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,43 @@
+// const express = require("express");
+// const app = express();
+// const cors = require("cors");
+// const tarefaRoutes = require("./routes/tarefaRoutes");
+// const sequelize = require("./database/db");
+// const Tarefa = require("./models/Tarefa");
+
+// app.use(cors());
+// app.use(express.json());
+// app.use(tarefaRoutes);
+
+// sequelize.sync().then(() => {
+//   app.listen(3000, () => {
+//     console.log("Servidor rodando em http://localhost:3000");
+//   });
+// });
+
 const express = require("express");
-const app = express();
 const cors = require("cors");
 const tarefaRoutes = require("./routes/tarefaRoutes");
-const sequelize = require("./database/db");
-const Tarefa = require("./models/Tarefa");
+const sequelize = require("./database/db")
+const app = express()
 
-app.use(cors());
-app.use(express.json());
-app.use(tarefaRoutes);
+//middleware//
 
-sequelize.sync().then(() => {
-  app.listen(3000, () => {
-    console.log("Servidor rodando em http://localhost:3000");
-  });
-});
+app.use(cors)
+app.use(express.json())
+
+//rotas
+app,use("/tarefas", tarefaRoutes)
+
+//sincronização com o banco - start do server
+sequelize.sync()
+ .then(()=> {
+  console.log("Banco de dados sincronizado com sucesso")
+  app.listen(3000, ()=> {
+    console.log("Servidor rodando em http://localhost:3000")
+  })
+ })
+
+ .catch((error) => {
+  console.error("Erro ao conectar o banco de dados", error)
+ })
